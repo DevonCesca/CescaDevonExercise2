f874cdb359194d174d01bcc7146e38aa58696a8c
Devon Cesca <dcesca@uncc.edu>
2022-01-29 21:59:08 -0500

    working update - push

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = 8080;
 const host = 'localhost';
 const server = http.createServer((req, res) => {
-    console.log(req);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile('./views/contact.html', (err, data) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.write(data);
+            res.end();
+        }
+    });
 });
 
 server.listen(port, host, () => {
